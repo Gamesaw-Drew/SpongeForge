3d094ad284cfc33f4a56493e84c7931bd334524b
Gabriel Harris-Rouquette <gabizou@me.com>
2017-04-07 15:15:23 -0700

    Add verification of ItemStack creation with an Item that may not be found in Forge registry.
    Signed-off-by: Gabriel Harris-Rouquette <gabizou@me.com>

--- src/main/java/org/spongepowered/mod/mixin/core/item/MixinItemStack.java
+++ src/main/java/org/spongepowered/mod/mixin/core/item/MixinItemStack.java
@@ -24,23 +24,41 @@
  */
 package org.spongepowered.mod.mixin.core.item;
 
+import com.google.common.primitives.Ints;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.stats.StatList;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
+import org.apache.logging.log4j.Level;
+import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.util.PrettyPrinter;
+import org.spongepowered.common.SpongeImpl;
+import org.spongepowered.common.registry.type.ItemTypeRegistryModule;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+import javax.annotation.Nullable;
 
 @Mixin(net.minecraft.item.ItemStack.class)
 public abstract class MixinItemStack {
 
-    @Shadow private net.minecraft.item.Item item;
+    @Shadow @Nullable private net.minecraft.item.Item item;
     @Shadow public abstract net.minecraft.item.Item getItem();
 
+    private static final Set<String> ITEM_IDS_NOT_FOUND = new ConcurrentSkipListSet<>();
     // Disable Forge PlaceEvent patch as we handle this in World setBlockState
 
     /**
@@ -70,5 +88,61 @@ public abstract class MixinItemStack {
         return result;
     }
 
+    @Redirect(method = "setItem", at = @At(value = "FIELD", target = "Lnet/minecraft/item/ItemStack;item:Lnet/minecraft/item/Item;", opcode = Opcodes.PUTFIELD))
+    private void spongeVerifyItemSet(ItemStack thisStack, Item item) {
+        if (item == null) {
+            this.item = null;
+            return;
+        } else if (item == ItemTypeRegistryModule.NONE_ITEM) {
+            this.item = ItemTypeRegistryModule.NONE_ITEM; // Sponge - we have to do this.
+            return;
+        }
+        final String id = item.getRegistryName().toString();
+        if (id.isEmpty()) {
+            // Means we found an empty item name....
+
+        }
+        Item foundItem = Item.REGISTRY.getObject(new ResourceLocation(id));
+
+        if (foundItem == null) {
+            try {
+                // Sponge Start - Use Ints.tryParse instead of Integer.parseInt to avoid an exception
+                // return getItemById(Integer.parseInt(id));
+                final Integer itemId = Ints.tryParse(id);
+                if (itemId == null) {
+                    // Since no exception was printed out, we can safely say
+                    // that the id is alphanumerical, not just numerical.
+                    if (!ITEM_IDS_NOT_FOUND.contains(id)) {
+                        ITEM_IDS_NOT_FOUND.add(id);
+                        // now we can print it to console
+                        new PrettyPrinter(60).add("Non Registered Item Id Found").centre().hr()
+                                .addWrapped("Sponge has detected that an invalid Item id has been found, attempting to "
+                                            + "be deserialized. The issue is that the Item by the id is not "
+                                            + "registered with Forge, which causes exceptions to be thrown "
+                                            + "when entities have that item added and when they are deserialized.")
+                                .add()
+                                .add("This is what Sponge found:")
+                                .add("%s : %s", "Item Id attempting to be located", id)
+                                .add("%s : %s", "Item instance", item)
+                                .add("Derp")
+                                .add()
+                                .add("Printing stacktrace")
+                                .add(new Exception())
+                                .trace(System.err, SpongeImpl.getLogger(), Level.ERROR);
+                    }
+                    this.item = null;
+                    return;
+                }
+                this.item = Item.getItemById(itemId);
+                // Sponge End
+            } catch (Exception e) {
+                // do nothing
+                this.item = null;
+                return;
+            }
+        }
+
+        this.item = foundItem;
+    }
 
 }
